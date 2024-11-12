d81e2ba29ea56904ea513b698b0814ee8b19748e
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2024-11-12 13:53:16 +0500

    chnage code

--- src/app/(other)/about/page.tsx
+++ src/app/(other)/about/page.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const About = () => {
   return (
     <main className="w-full px-4 md:px-8 lg:px-16 py-8">
-      <div className="max-w-screen-lg mx-auto border-t-2 border-b-2 border-gray-400 font-serif">
+      <div className="max-w-screen-lg mx-auto my-32 border-t-2 border-b-2 border-gray-400 font-serif">
         <div className="grid gap-8 md:gap-16 mb-8 md:mb-16 pt-8 md:pt-16 lg:pt-20 px-4 md:px-16 lg:px-24">
           <div>
-            <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-semibold">About Us</h1>
+            <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-[400px]">About Us</h1>
           </div>
           <div>
             <p className="text-center text-sm md:text-base lg:text-lg leading-relaxed">
--- src/app/components/Blogs.tsx
+++ src/app/components/Blogs.tsx
@@ -1,94 +1,35 @@
-// import React from 'react'
-// import Image from 'next/image'
-
-// const Blogs = () => {
-//   return (
-//     <main>
-//       {/* this component made for blog section in landind page */}
-//     <div className='w-full h-[623.3px]'>
-//       <div className='w-[993px] mt-[96px] ml-[130px]'>
-//         <h1 className='ml-[465.7px] text-[36px] mb-[64px]'>Blogs</h1>
-//         <section className='flex justify-around
-//         '>
-//         <div className="hover:-translate-y-1 ">
-//               <Image
-//                 src={"/first-square.png"}
-//                 alt="skin problem picture"
-//                 width={206}
-//                 height={305}
-//                 objectFit="cover"
-//               />
-//             </div>
-//         <div className="hover:-translate-y-1 ">
-//               <Image
-//                 src={"/second-square.png"}
-//                 alt="skin problem picture"
-//                 width={206}
-//                 height={305}
-//                 objectFit="cover"
-//               />
-//             </div>
-//         <div className="hover:-translate-y-1 ">
-//               <Image
-//                 src={"/third-square.png"}
-//                 alt="skin problem picture"
-//                 width={206}
-//                 height={305}
-//                 objectFit="cover"
-//               />
-//             </div>
-//         </section>
-//              <div className='mt-[155px] ml-[450px] mb-[197.97px]'>
-//               <button className='px-[50px] py-[10px] border-[1px] border-black '>More</button>
-//              </div>
-           
-//       </div>
-//     </div>
-//     </main>
-//   )
-// }
-
-// export default Blogs
-
-
 import React from 'react'
 import Image from 'next/image'
 
 const Blogs = () => {
   return (
     <main>
-      {/* This component made for the blog section on the landing page */}
-      <div className='w-full h-[623.3px]'>
-        <div className='w-full max-w-screen-xl mx-auto mt-[96px] px-4'>
-          <h1 className='text-center text-[36px] mb-[64px]'>Blogs</h1>
-
-          <section className='flex flex-col sm:flex-row sm:justify-between sm:gap-6'>
-            {/* Blog Image 1 */}
-            <div className="hover:-translate-y-1 mb-8 sm:mb-0 sm:w-1/3">
+      {/* Blog section for the landing page */}
+      <div className="w-full h-[624px]">
+        <div className="w-[90%] max-w-[993px] mt-24 mx-auto">
+          <h1 className="text-center text-[36px] mb-16">Blogs</h1>
+          <section className="flex justify-around">
+            <div className="hover:-translate-y-1 transition-transform">
               <Image
-                src={"/first-square.png"}
+                src="/first-square.png"
                 alt="skin problem picture"
                 width={206}
                 height={305}
                 objectFit="cover"
               />
             </div>
-
-            {/* Blog Image 2 */}
-            <div className="hover:-translate-y-1 mb-8 sm:mb-0 sm:w-1/3">
+            <div className="hover:-translate-y-1 transition-transform">
               <Image
-                src={"/second-square.png"}
+                src="/second-square.png"
                 alt="skin problem picture"
                 width={206}
                 height={305}
                 objectFit="cover"
               />
             </div>
-
-            {/* Blog Image 3 */}
-            <div className="hover:-translate-y-1 mb-8 sm:mb-0 sm:w-1/3">
+            <div className="hover:-translate-y-1 transition-transform">
               <Image
-                src={"/third-square.png"}
+                src="/third-square.png"
                 alt="skin problem picture"
                 width={206}
                 height={305}
@@ -96,12 +37,8 @@ const Blogs = () => {
               />
             </div>
           </section>
-
-          {/* Button */}
-          <div className='mt-[155px] text-center'>
-            <button className='px-[50px] py-[10px] border-[1px] border-black'>
-              More
-            </button>
+          <div className="mt-36 mb-48 text-center">
+            <button className="px-12 py-2 border border-black">More</button>
           </div>
         </div>
       </div>
